02912e659acc8094593360dab3865cb0943241f2
aquinovo <vela.aquino@gmail.com>
2017-12-12 17:59:31 -0600

    2

---
+++ funciones.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <map>
+#include <time.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <queue>
+#include <sys/time.h>
+#include <fstream>
+
+using namespace std;
+
