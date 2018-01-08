728c9efd46286b322d4e0a1e6de003347d9ec703
aquinovo <vela.aquino@gmail.com>
2018-01-08 17:31:19 -0600

    leer entrada

--- funciones.cpp
+++ funciones.cpp
@@ -9,6 +9,24 @@
 #include <queue>
 #include <sys/time.h>
 #include <fstream>
+#include <set>
 
 using namespace std;
+vector <int> crear_solucion(map<int, pair<int,int> > entrada, map< pair<int,int>,  pair<float,float> > distancias, int num_nodes){
+  vector <int> nodos_disponibles;
+  vector <int> solucion;
+  int nodo;
 
+  for (int i = 0; i < num_nodes; ++i){
+    nodos_disponibles.push_back(i+1);
+  }
+
+
+  while( nodos_disponibles.size()>=0 ){
+    nodo=rand()%nodos_disponibles.size();
+    solucion.push_back(nodos_disponibles[nodo]);
+    nodos_disponibles.erase(nodos_disponibles.begin()+nodo);
+  }
+
+  return solucion;
+}
--- tsp.cpp
+++ tsp.cpp
@@ -4,8 +4,9 @@
 int main() {
     int num_nodes,node;
     pair<int,int> coordenada,coordenada1,coordenada2;
+    pair<float,float> valores;
     map<int, pair<int,int> > entrada;
-    map< pair<int,int>, float > distancias;
+    map< pair<int,int>, pair<float,float> > distancias;
  
     cin >> num_nodes;
 
@@ -13,16 +14,29 @@ int main() {
  		 entrada[node]=coordenada;
 	}
 
-	for (int i = 1; i < entrada.size(); ++i){
-		for (int j = i+1; j < entrada.size()+1; ++j){
+	for (int i = 1; i < entrada.size(); i++){
+		for (int j = i+1; j < entrada.size()+1; j++){
             coordenada1.first=i; coordenada1.second=j; 
-            coordenada2.first=i; coordenada2.second=j; 
-            distancias[coordenada1]=sqrt( pow(entrada[i].first - entrada[j].first ,2.0) + pow(entrada[i].second - entrada[j].second,2.0) );
-            distancias[coordenada2]=distancias[coordenada1];
-		}
+            coordenada2.first=j; coordenada2.second=i; 
+            
+            valores.first= sqrt( pow(entrada[i].first - entrada[j].first ,2.0) + pow(entrada[i].second - entrada[j].second,2.0) );
+            valores.second= 1/sqrt( pow(entrada[i].first - entrada[j].first ,2.0) + pow(entrada[i].second - entrada[j].second,2.0) );
+
+            distancias[coordenada1]=valores;
+            distancias[coordenada2]=valores;
+            
+		}	
 	}
 
-	
+	for (int i = 1; i < 2; i++){
+		for (int j = i+1; j < 10; j++){
+			 coordenada1.first=i; coordenada1.second=j; 
+             cout<<distancias[coordenada1].first<<" -> "<<distancias[coordenada1].second<<endl;
+		}
+	}	
+
+    crear_solucion(entrada,distancias,num_nodes);
+
 
 	return 0;
 }
