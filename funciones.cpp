6e08bbef6f32687da97822299b4a0f26d6dd3ccb
aquinovo <vela.aquino@gmail.com>
2018-01-09 15:49:34 -0600

    movimiento-hormiga

--- funciones.cpp
+++ funciones.cpp
@@ -10,32 +10,66 @@
 #include <sys/time.h>
 #include <fstream>
 #include <set>
+#include <algorithm> 
 
 using namespace std;
 
-vector <float> evualuar_movimientos( vector <int> movimiento_posible,vector <int> solucion,map<int, pair<int,int> > entrada, map< pair<int,int>,  pair<float,float> > distancias, int nodo, int alfa, int beta){
+vector <float> evualuar_movimientos(vector <int> movimiento_posible, map< pair<int,int>,  pair<float,float> > distancias, int nodo, int alfa, int beta){
+      vector<float> Probabilidad;  
+      float probabilidad_i;
+      pair <int,int> relacion_ij;
+      pair <float,float> valores_ij; 
+      //obtener la sumatoria hacia los nodos posibles
+      float sumatoria=0;
+  
+      for (int i = 0; i < movimiento_posible.size(); ++i){
+         relacion_ij.first=nodo;
+         relacion_ij.second=movimiento_posible[i];
+
+         valores_ij=distancias[relacion_ij]; 
+         probabilidad_i=pow(valores_ij.second,alfa)*pow(valores_ij.first,beta);
+
+         sumatoria+=probabilidad_i;
+         Probabilidad.push_back( probabilidad_i );
+      }
       
-      //obtener la sumatoria del recorrido total    
+      for (int i = 0; i < movimiento_posible.size(); ++i){
+            Probabilidad[i]/=sumatoria;
+      }
+      return Probabilidad;
 }
 
-vector <int> hormiga(map<int, pair<int,int> > entrada, map< pair<int,int>,  pair<float,float> > distancias, int num_nodes, int alfa, int beta){
+bool myfunction (int i,int j) { return (i<j); }
+int movimiento_hormiga(vector<float> Probabilidad){
+  int pos=0;
+  sort(Probabilidad.begin(),Probabilidad.end(),myfunction);
+
+  for (int i = 0; i < Probabilidad.size(); ++i){
+    cout<<Probabilidad[i]<<endl;
+  }
+  return pos;
+}
+
+vector <int> hormiga(map< pair<int,int>,  pair<float,float> > distancias, int num_nodes, int alfa, int beta, int num_hormigas){
   vector <int> movimiento_posible;
+  vector<float> Probabilidad;
   vector <int> solucion;
-  int nodo,pos;
+  int nodo,pos_inicial;
 
   for (int i = 0; i < num_nodes; ++i){
     movimiento_posible.push_back(i+1);
   }
 
-  pos=rand() % movimiento_posible.size();
-  nodo=movimiento_posible[pos];
-  movimiento_posible.erase (movimiento_posible.begin()+pos);
-  
+  pos_inicial=rand() % movimiento_posible.size();
+  nodo=movimiento_posible[pos_inicial];
+  movimiento_posible.erase (movimiento_posible.begin()+pos_inicial);
   solucion.push_back(nodo);
 
-  evualuar_movimientos(movimiento_posible,solucion,entrada, distancias, nodo,alfa,beta);
 
-  
+
+  Probabilidad = evualuar_movimientos(movimiento_posible, distancias, nodo,alfa,beta);
+  movimiento_hormiga(Probabilidad); 
+    
 
 
 
--- tsp.cpp
+++ tsp.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     int num_nodes,node;
-    pair<int,int> coordenada,coordenada1,coordenada2;
+    pair<int,int> coordenada,relacion1,relacion2;
     pair<float,float> valores;
     map<int, pair<int,int> > entrada;
     map< pair<int,int>, pair<float,float> > distancias;
@@ -17,19 +17,18 @@ int main() {
 
 	for (int i = 1; i < entrada.size(); i++){
 		for (int j = i+1; j < entrada.size()+1; j++){
-            coordenada1.first=i; coordenada1.second=j; 
-            coordenada2.first=j; coordenada2.second=i; 
+            relacion1.first=i; relacion1.second=j; 
+            relacion2.first=j; relacion2.second=i; 
             
             valores.first= sqrt( pow(entrada[i].first - entrada[j].first ,2.0) + pow(entrada[i].second - entrada[j].second,2.0) );
             valores.second= 1/sqrt( pow(entrada[i].first - entrada[j].first ,2.0) + pow(entrada[i].second - entrada[j].second,2.0) );
 
-            distancias[coordenada1]=valores;
-            distancias[coordenada2]=valores;
-            
+            distancias[relacion1]=valores;
+            distancias[relacion2]=valores;
 		}	
 	}
-
-    hormiga(entrada,distancias,num_nodes,1,0.5);
+    
+    hormiga(distancias,num_nodes,1,0.5,5);
 	return 0;
 }
 
